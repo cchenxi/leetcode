18d68e18b81002853bda04b629368a1de27f38ac
chenxi <chenxi.et@gmail.com>
2019-04-19 08:38:33 +0800

    init database dir && first commit

---
+++ database/solution.sql
@@ -0,0 +1,73 @@
+-- db 181
+Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, ManagerId int);
+Truncate table Employee;
+insert into Employee (Id, Name, Salary, ManagerId) values ('1', 'Joe', '70000', '3');
+insert into Employee (Id, Name, Salary, ManagerId) values ('2', 'Henry', '80000', '4');
+insert into Employee (Id, Name, Salary, ManagerId) values ('3', 'Sam', '60000', NULL);
+insert into Employee (Id, Name, Salary, ManagerId) values ('4', 'Max', '90000', NULL);
+
+SELECT e1.Name as Employee FROM Employee e1, Employee e2 WHERE e1.ManagerId = e2.Id AND e1.Salary > e2.Salary;
+
+-- db 182
+Create table If Not Exists Person (Id int, Email varchar(255));
+Truncate table Person;
+insert into Person (Id, Email) values ('1', 'a@b.com');
+insert into Person (Id, Email) values ('2', 'c@d.com');
+insert into Person (Id, Email) values ('3', 'a@b.com');
+
+SELECT DISTINCT p1.Email FROM Person AS p1 JOIN Person AS p2 ON p1.Email = p2.Email AND p1.Id <> p2.Id;
+SELECT Email FROM Person GROUP BY Email HAVING COUNT(*) > 1;
+
+-- db 183
+Create table If Not Exists Customers (Id int, Name varchar(255));
+Create table If Not Exists Orders (Id int, CustomerId int);
+Truncate table Customers;
+insert into Customers (Id, Name) values ('1', 'Joe');
+insert into Customers (Id, Name) values ('2', 'Henry');
+insert into Customers (Id, Name) values ('3', 'Sam');
+insert into Customers (Id, Name) values ('4', 'Max');
+Truncate table Orders;
+insert into Orders (Id, CustomerId) values ('1', '3');
+insert into Orders (Id, CustomerId) values ('2', '1');
+
+SELECT t1.`Name` AS Customers FROM Customers t1 WHERE t1.Id NOT	IN (SELECT Orders.CustomerId from Orders);
+
+-- db 196
+delete p1 from Person p1, Person p2 where p1.Email = p2.Email and p1.Id > p2.Id;
+delete p1 from Person p1 JOIN Person p2 ON p1.Email = p2.Email and p1.Id > p2.Id;
+
+-- db 197
+Create table If Not Exists Weather (Id int, RecordDate date, Temperature int);
+Truncate table Weather;
+insert into Weather (Id, RecordDate, Temperature) values ('1', '2015-01-01', '10');
+insert into Weather (Id, RecordDate, Temperature) values ('2', '2015-01-02', '25');
+insert into Weather (Id, RecordDate, Temperature) values ('3', '2015-01-03', '20');
+insert into Weather (Id, RecordDate, Temperature) values ('4', '2015-01-04', '30');
+
+SELECT t1.Id FROM Weather t1 JOIN Weather t2 ON t1.Temperature > t2.Temperature AND DATEDIFF(t1.RecordDate,t2.RecordDate) = 1;
+
+-- db 595
+Create table If Not Exists World (name varchar(255), continent varchar(255), area int, population int, gdp BIGINT);
+Truncate table World;
+insert into World (name, continent, area, population, gdp) values ('Afghanistan', 'Asia', '652230', '25500100', '20343000000');
+insert into World (name, continent, area, population, gdp) values ('Albania', 'Europe', '28748', '2831741', '12960000000');
+insert into World (name, continent, area, population, gdp) values ('Algeria', 'Africa', '2381741', '37100000', '188681000000');
+insert into World (name, continent, area, population, gdp) values ('Andorra', 'Europe', '468', '78115', '3712000000');
+insert into World (name, continent, area, population, gdp) values ('Angola', 'Africa', '1246700', '20609294', '100990000000');
+
+SELECT World.`name`, World.population, World.area FROM World WHERE World.area > 3000000 OR World.population > 25000000;
+
+-- db 596
+Create table If Not Exists courses (student varchar(255), class varchar(255));
+Truncate table courses;
+insert into courses (student, class) values ('A', 'Math');
+insert into courses (student, class) values ('B', 'English');
+insert into courses (student, class) values ('C', 'Math');
+insert into courses (student, class) values ('D', 'Biology');
+insert into courses (student, class) values ('E', 'Math');
+insert into courses (student, class) values ('F', 'Computer');
+insert into courses (student, class) values ('G', 'Math');
+insert into courses (student, class) values ('H', 'Math');
+insert into courses (student, class) values ('I', 'Math');
+
+SELECT class FROM courses GROUP BY class HAVING COUNT(DISTINCT student) >= 5;
