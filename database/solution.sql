da1bd8f5f6b430acb73375749d440d7737395c03
chenxi <chenxi.et@gmail.com>
2019-04-19 08:53:20 +0800

    add db 620

--- database/solution.sql
+++ database/solution.sql
@@ -71,3 +71,15 @@ insert into courses (student, class) values ('H', 'Math');
 insert into courses (student, class) values ('I', 'Math');
 
 SELECT class FROM courses GROUP BY class HAVING COUNT(DISTINCT student) >= 5;
+
+-- db 620
+Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1));
+Truncate table cinema;
+insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9');
+insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5');
+insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2');
+insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6');
+insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1');
+
+	-- 奇数 num&1 https://blog.csdn.net/zhazhagu/article/details/80452473
+SELECT * FROM cinema WHERE cinema.id&1 AND cinema.description != 'boring' ORDER BY cinema.rating DESC;
