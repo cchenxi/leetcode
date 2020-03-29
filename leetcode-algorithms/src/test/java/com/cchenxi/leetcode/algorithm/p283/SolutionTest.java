6f42e7babdcb669db12981c0231bc5db134b4a64
chenxi <chenxi.et@gmail.com>
2020-03-29 21:37:12 +0800

    p283 移动0

---
+++ leetcode-algorithms/src/test/java/com/cchenxi/leetcode/algorithm/p283/SolutionTest.java
@@ -0,0 +1,20 @@
+package com.cchenxi.leetcode.algorithm.p283;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * TODO: description
+ * Date: 2020-03-29
+ *
+ * @author chenxi
+ */
+public class SolutionTest {
+
+    @Test
+    public void moveZeroes() {
+        Solution solution = new Solution();
+        solution.moveZeroes(new int[]{1, 0, 3, 100, 0, 21});
+    }
+}
