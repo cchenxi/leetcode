d1ff6e31fe7b40b5eb9c3159c9259dcf2ad7798a
chenxi <chenxi.et@gmail.com>
2020-07-24 09:13:05 +0800

    237 删除排序链表中的节点 83 删除排序链表中的重复节点

---
+++ leetcode-algorithms/src/main/java/com/cchenxi/leetcode/algorithm/n83/ListNode.java
@@ -0,0 +1,16 @@
+package com.cchenxi.leetcode.algorithm.n83;
+
+/**
+ * Date: 2020-07-22
+ *
+ * @author chenxi
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
