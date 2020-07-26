64f64ef71a4a7d2a0bea81473bf3bd4a1e840a5c
chenxi <chenxi.et@gmail.com>
2020-07-26 19:44:14 +0800

    189 旋转数组

--- leetcode-algorithms/src/main/java/com/cchenxi/leetcode/algorithm/n189/RotateArray.java
+++ leetcode-algorithms/src/main/java/com/cchenxi/leetcode/algorithm/n189/RotateArray.java
@@ -33,7 +33,7 @@ package com.cchenxi.leetcode.algorithm.n189;
  */
 public class RotateArray {
     public void rotate(int[] nums, int k) {
-        m1(nums, k);
+        m3(nums, k);
     }
 
     /**
@@ -72,8 +72,20 @@ public class RotateArray {
         System.arraycopy(newNums, 0, nums, 0, nums.length);
     }
 
-    //环状替代 && 反转
     public void m3(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
 
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            left++;
+            right--;
+        }
     }
 }
