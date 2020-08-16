0911914ba3ec0cdf13544b6a77003a17923f8df3
chenxi <chenxi.et@gmail.com>
2020-08-16 20:52:25 +0800

    冒泡排序

---
+++ leetcode-algorithms/src/main/java/com/cchenxi/leetcode/algorithm/sort/BubbleSort.java
@@ -0,0 +1,25 @@
+package com.cchenxi.leetcode.algorithm.sort;
+
+/**
+ * 冒泡排序
+ * Date: 2020-08-16
+ *
+ * @author chenxi
+ */
+public class BubbleSort {
+    public void sort(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+}
