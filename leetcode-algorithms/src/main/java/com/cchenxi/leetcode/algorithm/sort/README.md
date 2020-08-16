cb4aca3bfeef88181b3b7ace8491524e4c79ce20
chenxi <chenxi.et@gmail.com>
2020-08-16 20:48:07 +0800

    插入排序

--- leetcode-algorithms/src/main/java/com/cchenxi/leetcode/algorithm/sort/README.md
+++ leetcode-algorithms/src/main/java/com/cchenxi/leetcode/algorithm/sort/README.md
@@ -1,1 +1,3 @@
 各种排序算法
+
+参考 https://www.cnblogs.com/onepixel/p/7674659.html
---
+++ leetcode-algorithms/src/test/java/com/cchenxi/leetcode/algorithm/sort/InsertionSortTest.java
@@ -0,0 +1,30 @@
+package com.cchenxi.leetcode.algorithm.sort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Date: 2020-08-16
+ *
+ * @author chenxi
+ */
+public class InsertionSortTest {
+    private InsertionSort c;
+    private int[] array;
+
+    @Before
+    public void setUp() throws Exception {
+        c = new InsertionSort();
+        array = new int[]{1, 2, 3, 7, 6, 10, 25, 9, 11};
+    }
+
+    @Test
+    public void sort() {
+        c.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
