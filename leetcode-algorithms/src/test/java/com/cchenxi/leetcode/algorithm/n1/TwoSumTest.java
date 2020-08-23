e76c8f47f65a11ec9733e2282683155db368894e
chenxi <chenxi.et@gmail.com>
2020-08-23 22:27:21 +0800

    实验新的LeetCode刷题模板 题目采用interface形式，每一种解法用class实现interface 如interface TwoSum 解题方法1 Solution1 implements TwoSum override twoSum方法 解题方法2 Solution2 implements TwoSum override twoSum方法
    单元测试 TwoSum c = new Solution1();
    
    n遍刷题，则新建Solution1n 1表示解题方法 n表示第n遍，当然n刷的时候不一定使用几种方法，可以自行决定

--- leetcode-algorithms/src/test/java/com/cchenxi/leetcode/algorithm/n1/TwoSumTest.java
+++ leetcode-algorithms/src/test/java/com/cchenxi/leetcode/algorithm/n1/TwoSumTest.java
@@ -1,18 +1,35 @@
 package com.cchenxi.leetcode.algorithm.n1;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
- * Date: 2020-06-27
+ * TODO: description
+ * Date: 2020-08-23
  *
  * @author chenxi
  */
 public class TwoSumTest {
+    private int[] nums;
+    @Before
+    public void setUp() throws Exception {
+        nums = new int[]{2, 7, 11, 15, 5};
+    }
 
     @Test
     public void twoSum() {
-        TwoSum c = new TwoSum();
-        int[] result = c.twoSum(new int[]{2, 7, 11, 15, 5}, 20);
-        System.out.println(result);
+        TwoSum c = new Solution();
+        int[] result = c.twoSum(nums, 9);
+        Assert.assertArrayEquals(new int[]{0, 1}, result);
+    }
+
+    @Test
+    public void twoSum2() {
+        TwoSum c = new Solution2();
+        int[] result = c.twoSum(nums, 9);
+        Assert.assertArrayEquals(new int[]{1, 0}, result);
     }
 }
