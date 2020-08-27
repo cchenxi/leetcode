4b3c4486b020012b35b2b45fd6d14c690be72975
chenxi <chenxi.et@gmail.com>
2020-08-27 19:03:40 +0800

    102 BinaryTreeLevelOrderTraversal 2_1 不要忘记加入左右子树的时候判空

---
+++ leetcode-algorithms/src/main/java/com/cchenxi/leetcode/algorithm/n102BinaryTreeLevelOrderTraversal/Solution_2_1.java
@@ -0,0 +1,44 @@
+package com.cchenxi.leetcode.algorithm.n102BinaryTreeLevelOrderTraversal;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Date: 2020-08-27
+ *
+ * @author chenxi
+ */
+public class Solution_2_1 implements BinaryTreeLevelOrderTraversal {
+    @Override
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        List<List<Integer>> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                level.add(current.val);
+                //忘记判断非空
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+            result.add(level);
+        }
+
+        return result;
+    }
+}
