811ecd259239bc363571c80807720d1446ef45b2
chenxi <chenxi.et@gmail.com>
2021-08-04 22:43:01 +0800

    leetcode 283 移动零 *2

---
+++ trusted-exam/src/main/java/io/github/cchenxi/leetcode/p283/Solution02.java
@@ -0,0 +1,24 @@
+package io.github.cchenxi.leetcode.p283;
+
+/**
+ * Date: 2021-08-04
+ *
+ * @author chenxi
+ */
+public class Solution02 {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+}
