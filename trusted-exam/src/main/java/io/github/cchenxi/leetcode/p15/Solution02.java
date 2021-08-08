1839861442a502c95b2a4439c0faf85ce5d8e23f
chenxi <chenxi.et@gmail.com>
2021-08-08 16:45:44 +0800

    leetcode 15 三数之和 *2

--- trusted-exam/src/main/java/io/github/cchenxi/leetcode/p15/Solution02.java
+++ trusted-exam/src/main/java/io/github/cchenxi/leetcode/p15/Solution02.java
@@ -5,11 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * @author chenxi
- * @see <a href="https://leetcode-cn.com/problems/3sum/">三数之和</a>
+ * {@link Solution01}
  * Date: 2021-08-08
+ *
+ * @author chenxi
  */
-public class Solution01 {
+public class Solution02 {
     public List<List<Integer>> threeSum(int[] nums) {
         if (nums == null || nums.length < 3) {
             return new ArrayList<>();
@@ -28,20 +29,16 @@ public class Solution01 {
 
             int left = i + 1;
             int right = nums.length - 1;
-
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum > 0) {
-                    while (left < right && nums[right] == nums[--right]);
-                } else if (sum < 0) {
+                if (sum < 0) {
                     while (left < right && nums[left] == nums[++left]);
+                } else if (sum > 0) {
+                    while (left < right && nums[right] == nums[--right]);
                 } else {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    // 双指针 同时向左和向右移动才有可能使下一个和=0
-                    // 单纯向右 会使和减小
-                    while (left < right && nums[right] == nums[--right]);
-                    // 单纯向左 会使和增大
                     while (left < right && nums[left] == nums[++left]);
+                    while (left < right && nums[right] == nums[--right]);
                 }
             }
         }
