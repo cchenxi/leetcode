6e715e4d1b455727b8d01990b0c65dbe4db3338f
chenxi <chenxi.et@gmail.com>
2021-08-08 19:11:02 +0800

    leetcode 15 最接近的三数之和 *1

---
+++ trusted-exam/src/test/java/io/github/cchenxi/leetcode/p16/Solution01Test.java
@@ -0,0 +1,28 @@
+package io.github.cchenxi.leetcode.p16;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Date: 2021-08-08
+ *
+ * @author chenxi
+ */
+public class Solution01Test {
+    private Solution01 solution;
+    private int[] nums;
+    private int target = 1;
+
+    @Before
+    public void setUp() {
+        solution = new Solution01();
+        nums = new int[]{0, 2, 1, -3};
+    }
+
+    @Test
+    public void threeSum() {
+        System.out.println(
+                solution.threeSumClosest(nums, target)
+        );
+    }
+}
