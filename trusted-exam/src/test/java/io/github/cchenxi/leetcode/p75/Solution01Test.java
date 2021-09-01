481229483a3f5ce343fc71c2b2e10d0cf289b108
chenxi <chenxi.et@gmail.com>
2021-09-01 23:48:04 +0800

    leetcode 75 颜色分类 215 数组中的第K个最大元素 643 子数组最大平均数 I

---
+++ trusted-exam/src/test/java/io/github/cchenxi/leetcode/p75/Solution01Test.java
@@ -0,0 +1,27 @@
+package io.github.cchenxi.leetcode.p75;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Date: 2021-08-29
+ *
+ * @author chenxi
+ */
+public class Solution01Test {
+    private Solution01 solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution01();
+    }
+
+    @Test
+    public void sortColors() {
+        int[] nums = new int[]{2,0,2,1,1,0, 2};
+        solution.sortColors(nums);
+        System.out.println(nums);
+    }
+}
